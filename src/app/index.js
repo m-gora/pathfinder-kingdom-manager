3cdf7892f02c777177394322c23c0f043962d965
Marco G <blackanthrax666@gmail.com>
2019-08-17 14:53:29 +0200

    checking out how material-ui works

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button'
-import Container from '@material-ui/core/Container'
-import KingdomInfo from '../components/kingdom-info'
 
-function App() {
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles(theme => ({
+  absolute: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(3),
+  },
+}));
+
+export default function App() {
+
+  const classes = useStyles();
+
   return (
-    <Container fixed>
-      <KingdomInfo />
-      <Button variant="contained" color="primary">Hello World</Button>
-    </Container>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="Open Drawer">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">Kingdom Manager</Typography>
+        </Toolbar>
+      </AppBar>
+      <Tooltip title="add" aria-label="add">
+        <Fab color="secondary" className={classes.absolute}>
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+    </React.Fragment>
   );
 }
 
--- src/components/kingdom-info.js
+++ src/components/kingdom-info.js
@@ -37,5 +37,4 @@ export default class KingdomInfo extends React.Component {
       )
   }
 
-
 }
